3fe50e6d475dc9f75724a4765ac6959153e82052
Coxhuang <mhesat@outlook.com>
2020-02-11 00:26:39 +0800

    update

--- insert_sort/shells_method/shells_method.py
+++ insert_sort/shells_method/shells_method.py
@@ -14,18 +14,17 @@ class ShellsMethodCls(object):
         # 初始化gap值，此处利用序列长度的一般为其赋值
         gap = (int)(len(target) / 2)
         # 第一层循环：依次改变gap值对列表进行分组
-        while (gap >= 1):
-            # 下面：利用直接插入排序的思想对分组数据进行排序
-            # range(gap,len(target)):从gap开始
-            for x in range(gap, len(target)):
-                # range(x-gap,-1,-gap):从x-gap开始与选定元素开始倒序比较，每个比较元素之间间隔gap
-                for i in range(x - gap, -1, -gap):
-                    # 如果该组当中两个元素满足交换条件，则进行交换
-                    if target[i] > target[i + gap]:
-                        temp = target[i + gap]
-                        target[i + gap] = target[i]
-                        target[i] = temp
-                        # while循环条件折半
-            gap = (int)(gap / 2)
+        while gap >= 1:
+            for x in range(len(target)):
+                y = x
+                while y >= gap and target[y - gap] > target[y]:  # 在每一组里面进行直接插入排序
+                    target[y], target[y - gap] = target[y - gap], target[y]
+                    y -= gap
+            gap //= 2  # 更新步长
             
         return target
+
+if __name__ == "__main__":
+    oby = ShellsMethodCls()
+    ret = oby.shells_method([9,8,7,6,5,4,3,2,1,0,-1,-2,1,2,3,6,7,8,9])
+    print(ret)
